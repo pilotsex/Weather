2b47cf0f8cdff7209a29945ee411899444f85a09
csaba_k <askme>
2019-03-30 15:21:13 +0100

    Editable (delete/reorder) city list and 'add new city' added. Nobody has really asked for it, but I added'em anyway :)

--- Weather/CitiesViewController.swift
+++ Weather/CitiesViewController.swift
@@ -18,7 +18,7 @@ struct CityCellData : Codable {
 
 class CitiesViewController: UITableViewController {
 
-    var cityCellData : Array<CityCellData>
+    private var cityCellData : Array<CityCellData>
     
     
     required init?(coder aDecoder: NSCoder) {
@@ -33,7 +33,16 @@ class CitiesViewController: UITableViewController {
     }
     
     
-    func loadCities() -> [CityCellData]? {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Weather"
+        //TODO the placements of the Edit/Add buttons are a little unconventional, I sould move the Add button to the footer or something
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addCity))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain , target: self, action: #selector(editTable))
+    }
+    
+    
+    private func loadCities() -> [CityCellData]? {
         if let loadedCellData = UserDefaults.standard.data(forKey: "Cities") {
             print("successfully loaded from defaults")
             if let decodedData = try? JSONDecoder().decode(Array<CityCellData>.self, from: loadedCellData) {
@@ -44,7 +53,7 @@ class CitiesViewController: UITableViewController {
     }
     
     
-    func saveCities() {
+    private func saveCities() {
         if let data = try? JSONEncoder().encode(cityCellData) {
             UserDefaults.standard.set(data, forKey: "Cities")
         }
@@ -52,24 +61,19 @@ class CitiesViewController: UITableViewController {
     }
     
     
-    func defaultCities() -> [CityCellData] {
+    private func defaultCities() -> [CityCellData] {
         return
             [CityCellData(cityName: "Sopron", countyName: "Gy-M-S megye", countryName: "hu"),
              CityCellData(cityName: "Győr", countyName: "Gy-M-S megye", countryName: "hu"),
              CityCellData(cityName: "Budapest", countyName: "Pest megye", countryName: "hu"),
              CityCellData(cityName: "Mucsa", countyName: "Nonexistent megye", countryName: "hu")]
     }
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cityCellData.count
     }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
@@ -78,5 +82,71 @@ class CitiesViewController: UITableViewController {
         return cell
     }
     
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert {
+            print("insert")
+        }
+        if editingStyle == .delete {
+            print("delete")
+        }
+        if editingStyle == .delete {
+            cityCellData.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        saveCities()
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedCellData = cityCellData[sourceIndexPath.row]
+        cityCellData.remove(at: sourceIndexPath.row)
+        cityCellData.insert(movedCellData, at: destinationIndexPath.row)
+        saveCities()
+    }
+    
+    
+    //dummy method for validating the name of the new city for existence
+    //we should perform a test query or use some more specific api call for a proper name validation
+    private func validateNewCity(cityName: String) -> Bool {
+        return true
+    }
+    
+    
+    @objc private func addCity() {
+        let actionSheet = UIAlertController(title: "Add new city", message: "Please enter the name of the city", preferredStyle: .alert)
+        actionSheet.addTextField()
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        actionSheet.addAction(UIAlertAction(title: "Add", style: .default) {[unowned self](alertAction) in
+            let textFieldText = actionSheet.textFields?[0].text
+            if let trimmedTextFieldText = textFieldText?.trimmingCharacters(in: .whitespacesAndNewlines) {
+                if self.validateNewCity(cityName: trimmedTextFieldText) {
+                    self.cityCellData.append(CityCellData(cityName: trimmedTextFieldText.lowercased().capitalized(with: Locale.current), countyName: nil, countryName: nil))
+                    self.tableView.reloadData()
+                    self.saveCities()
+                }
+                self.dismiss(animated: true)
+            }
+        })
+
+        present(actionSheet, animated: true)
+    }
+    
+    
+    @objc private func editTable(source: UIBarButtonItem) {
+        tableView.setEditing(!tableView.isEditing, animated: true)
+        source.title = tableView.isEditing ? "Done" : "Edit"
+        
+    }
 }
 
