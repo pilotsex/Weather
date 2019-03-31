cb9f66c8c853753e0ac5c2608ed286b824dd5ee2
csaba_k <askme>
2019-04-01 00:17:47 +0200

    cleaned up JSON structs

--- Weather/CurrentWeather.swift
+++ Weather/CurrentWeather.swift
@@ -12,14 +12,12 @@ import Foundation
 struct CurrentWeather: Codable {
     let coord: Coord
     let weather: [Weather]
-    let base: String
     let main: Main
     let visibility: Double?
     let wind: Wind
     let clouds: Clouds
     let dt: Int
     let sys: Sys
-    let id: Int
     let name: String
     let cod: Int
     
--- Weather/Forecast.swift
+++ Weather/Forecast.swift
@@ -9,76 +9,19 @@
 import Foundation
 
 
-
 //this is the structure of OWM's 'Forecast' JSON response
 struct Forecast: Codable {
     let cod: String
-    let message: Double
-    let cnt: Int
     let list: [List]
-    let city: City
-    
-    struct City: Codable {
-        let id: Int
-        let name: String
-        let coord: Coord
-        let country: String
-        let population: Double
-    }
-    
-    struct Coord: Codable {
-        let lat, lon: Double
-    }
     
     struct List: Codable {
         let dt: Double
         let main: MainClass
         let weather: [Weather]
-        let clouds: Clouds
-        let wind: Wind
-        let sys: Sys
-        let dtTxt: String
-        let rain: Rain?
-        
-        enum CodingKeys: String, CodingKey {
-            case dt, main, weather, clouds, wind, sys
-            case dtTxt = "dt_txt"
-            case rain
-        }
-    }
-    
-    struct Clouds: Codable {
-        let all: Double
     }
     
     struct MainClass: Codable {
-        let temp, tempMin, tempMax, pressure: Double
-        let seaLevel, grndLevel: Double
-        let humidity: Double
-        let tempKf: Double
-        
-        enum CodingKeys: String, CodingKey {
-            case temp
-            case tempMin = "temp_min"
-            case tempMax = "temp_max"
-            case pressure
-            case seaLevel = "sea_level"
-            case grndLevel = "grnd_level"
-            case humidity
-            case tempKf = "temp_kf"
-        }
-    }
-    
-    struct Rain: Codable {
-        let the3H: Double?
-        
-        enum CodingKeys: String, CodingKey {
-            case the3H = "3h"
-        }
-    }
-    
-    struct Sys: Codable {
-        let pod: String
+        let temp: Double
     }
     
     struct Weather: Codable {
@@ -86,10 +29,6 @@ struct Forecast: Codable {
         let main: String
         let description: String
         let icon: String
-    }
-    
-    struct Wind: Codable {
-        let speed, deg: Double
     }
 }
 
